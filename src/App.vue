893b6e902fb849b5fba4f20c2dde7f98ece0d230
stomita083 <s-tomita@unext.jp>
2019-10-03 20:34:31 +0900

    add auth

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
@@ -18,6 +18,42 @@
 }
 #nav {
   padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>-->
+
+<template>
+  <el-container id="app">
+    <el-header id="nav"><global-header/></el-header>
+    <el-main><router-view/></el-main>
+  </el-container>
+</template>
+
+<script>
+  import GlobalHeader from './components/GlobalHeader'
+  export default {
+    components: {
+      GlobalHeader
+    }
+  }
+
+</script>
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 10px;
   a {
     font-weight: bold;
     color: #2c3e50;
--- src/main.js
+++ src/main.js
@@ -3,12 +3,24 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import ElementUI from 'element-ui' // 追記
-import locale from 'element-ui/lib/locale/lang/ja' // 追記
-import 'element-ui/lib/theme-chalk/index.css' // 追記
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI, {locale}) // 追記
+Vue.use(ElementUI, {locale})
+
+var config = {
+    apiKey: "************************",
+    authDomain: "************************.firebaseapp.com",
+    databaseURL: "************************.firebaseio.com",
+    projectId: "************************",
+    storageBucket: "************************.appspot.com",
+    messagingSenderId: "************************"
+  };
+firebase.initializeApp(config);
 
 new Vue({
     router,
