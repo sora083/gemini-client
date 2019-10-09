541d06be1db5402d0cb402bac525154e2abefbc7
Sora Tomita <sora.tomita083@gmail.com>
2019-10-09 23:08:28 +0900

    change to use env

--- README.md
+++ README.md
@@ -1,29 +1,41 @@
 # gemini-client
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn run serve
+
+npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn run build
 ```
 
 ### Run your tests
+
 ```
 yarn run test
 ```
 
 ### Lints and fixes files
+
 ```
 yarn run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### reference
+
+[SpringBoot+Vue.js+ElementUI+Firebase でマスタ管理アプリ入門](https://qiita.com/shunp/items/abea7fa01e7a664c85da)
--- src/main.js
+++ src/main.js
@@ -1,29 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/ja'
-import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/ja";
+import "element-ui/lib/theme-chalk/index.css";
 
-import firebase from 'firebase'
+import firebase from "firebase";
+import dotenv from "dotenv";
 
-Vue.config.productionTip = false
-Vue.use(ElementUI, {locale})
+Vue.config.productionTip = false;
+Vue.use(ElementUI, { locale });
+
+const conf = dotenv.config();
+if (conf.error) {
+  console.log("Could not load env file", conf.error);
+}
 
 var config = {
-    apiKey: "************************",
-    authDomain: "************************.firebaseapp.com",
-    databaseURL: "************************.firebaseio.com",
-    projectId: "************************",
-    storageBucket: "",
-    messagingSenderId: ""
-  };
+  apiKey: process.env.VUE_APP_GEMINI_API_KEY,
+  authDomain: process.env.VUE_APP_GEMINI_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_GEMINI_DB_URL,
+  projectId: process.env.VUE_APP_GEMINI_PROJECT_ID,
+  storageBucket: "",
+  messagingSenderId: ""
+};
 firebase.initializeApp(config);
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
